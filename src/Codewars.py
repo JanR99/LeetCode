2b5df86444053a96400680765ce45f422044df36
Jan <jan.rehnert@fau.de>
2023-06-01 17:23:53 +0200

    completed "Jenny's secret message"

--- src/Codewars.py
+++ src/Codewars.py
@@ -1,3 +1,8 @@
 def reverse_seq(n):
     return list(range(n, 0, -1))
 
+
+def greet(name):
+    if name == "Johnny":
+        return "Hello, my love!"
+    return "Hello, {name}!".format(name=name)
