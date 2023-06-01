4bdd7a6701d24cfcd68d25d4a612333d8dd8b86f
Jan <jan.rehnert@fau.de>
2023-06-01 17:51:05 +0200

    completed "Pillars"

--- src/Codewars.py
+++ src/Codewars.py
@@ -12,3 +12,14 @@ def array_plus_array(arr1, arr2):
     return sum(arr1) + sum(arr2)
 
 
+def pillars(num_pill, dist, width):
+    if num_pill == 1:
+        return 0
+    ans = 0
+    if num_pill > 2:
+        ans += int(((num_pill - 2) * width))
+    return int(ans + (num_pill - 1) * dist * 100)
+
+
+if __name__ == '__main__':
+    print(pillars(2, 20, 25))
