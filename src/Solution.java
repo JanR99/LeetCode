4b176e4be375be006f0ba286383153eb33c6f48c
Jan <jan.rehnert@fau.de>
2023-06-21 09:44:49 +0200

    completed "Find Pivot Index"

--- src/Solution.java
+++ src/Solution.java
@@ -578,4 +578,18 @@ class Solution {
         }
         return ans;
     }
+
+    public int pivotIndex(int[] nums) {
+        int sum = 0;
+        for(int num : nums)
+            sum += num;
+        int leftSum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            int rightSum = sum - leftSum - nums[i];
+            if(leftSum == rightSum)
+                return i;
+            leftSum += nums[i];
+        }
+        return -1;
+    }
 }
