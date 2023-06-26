25b9bcbe821d8f13c6da9fff79d5a4f29a4c3fba
Jan <jan.rehnert@fau.de>
2023-06-26 15:35:28 +0200

    completed "Find the Distance Value Between Two Arrays"

--- src/Solution.java
+++ src/Solution.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
 
     public static void main(String[] args) {
+        System.out.println(new Solution().findTheDistanceValue(new int[]{4,5,8}, new int[]{10,9,1,8}, 2));
     }
 
 
@@ -641,5 +642,20 @@ class Solution {
             }
             return minHeap.peek();
         }
+    }
+
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        int ans = 0;
+        for(int i = 0; i < arr1.length; i++) {
+            boolean ok = true;
+            for(int j = 0; j < arr2.length; j++) {
+                if(Math.abs(arr1[i] - arr2[j]) <= d) {
+                    ok = false;
+                    break;
+                }
+            }
+            if(ok) ans++;
+        }
+        return ans;
     }
 }
