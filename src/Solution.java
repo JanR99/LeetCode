e5df973479e801c95a6d505f1d6b6212a0697589
Jan <jan.rehnert@fau.de>
2023-06-27 14:00:53 +0200

    completed "Find Maximum Number of String Pairs"

--- src/Solution.java
+++ src/Solution.java
@@ -767,4 +767,17 @@ class Solution {
         if(b == 0) return a;
         return countBeautifulPairsHelper(b, a % b);
     }
+
+    public int maximumNumberOfStringPairs(String[] words) {
+        int ans = 0;
+        for(int i = 0; i < words.length; i++) {
+            for(int j = i + 1; j < words.length; j++) {
+                if(words[i].equals(new StringBuilder(words[j]).reverse().toString()))
+                    ans++;
+            }
+        }
+        return ans;
+    }
+
+
 }
