3b025ddfb48bda5ed134970ff08996dcee8c7fa2
Jan <jan.rehnert@fau.de>
2023-08-01 14:37:09 +0200

    completed "Minimum Number of Moves to Seat Everyone"

--- src/Solution.java
+++ src/Solution.java
@@ -1722,4 +1722,14 @@ class Solution {
         list.add(node);
         increasingBSTHelper(node.right, list);
     }
+
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int ans = 0;
+        for(int i = 0; i < seats.length; i++) {
+            ans += Math.abs(students[i] - seats[i]);
+        }
+        return ans;
+    }
 }
