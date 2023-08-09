1c4b1321919a409f847ee24d4402df275ff48fc6
Jan <jan.rehnert@fau.de>
2023-08-09 11:34:30 +0200

    completed "Find Subarrays With Equal Sum"

--- src/Solution.java
+++ src/Solution.java
@@ -6,7 +6,7 @@ import java.util.*;
 class Solution {
 
     public static void main(String[] args) {
-
+        System.out.println(new Solution().findSubarrays(new int[]{1,2,3,4,5}));
     }
 
 
@@ -1801,5 +1801,16 @@ class Solution {
         else
             tmp = purchaseAmount + (10 - lastDigit);
         return 100 - tmp;
+    }
+
+    public boolean findSubarrays(int[] nums) {
+        for(int i = 0; i < nums.length - 1; i++) {
+            int front = nums[i] + nums[i + 1];
+            for(int j = i + 1; j < nums.length - 1; j++) {
+                int back = nums[j] + nums[j + 1];
+                if(back == front) return true;
+            }
+        }
+        return false;
     }
 }
