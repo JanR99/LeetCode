383876d18b038e56934aa4d16a3caef97f5cc71e
Jan <jan.rehnert@fau.de>
2023-08-21 19:28:33 +0200

    completed "Count Pairs Whose Sum is Less than Target"

--- src/Solution.java
+++ src/Solution.java
@@ -2032,4 +2032,16 @@ class Solution {
             if (words.get(i).charAt(0) != s.charAt(i)) return false;
         return true;
     }
+
+    public int countPairs(List<Integer> nums, int target) {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums.get(i) + nums.get(j) < target) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
 }
