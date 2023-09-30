4299a4ce3123c388b43839dc437fa9c8eed1ac22
Jan <jan.rehnert@fau.de>
2023-09-30 11:51:24 +0200

    completed "132 Pattern"

--- src/Solution.java
+++ src/Solution.java
@@ -6,7 +6,7 @@ import java.util.*;
 class Solution {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().minSubsequence(new int[]{4,3,10,9,8}));
+        System.out.println(new Solution().find132pattern(new int[]{3,1,4,2}));
     }
 
 
@@ -2622,5 +2622,25 @@ class Solution {
             }
         }
         return ans;
+    }
+
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        if (n < 3) return false;
+        int[] minArray = new int[n];
+        minArray[0] = nums[0];
+        for (int i = 1; i < n; i++)
+            minArray[i] = Math.min(minArray[i - 1], nums[i]);
+        Stack<Integer> stack = new Stack<>();
+        for (int j = n - 1; j >= 0; j--) {
+            if (nums[j] > minArray[j]) {
+                while (!stack.isEmpty() && stack.peek() <= minArray[j])
+                    stack.pop();
+                if (!stack.isEmpty() && stack.peek() < nums[j])
+                    return true;
+                stack.push(nums[j]);
+            }
+        }
+        return false;
     }
 }
