da96862944502d4aa05d6c02cd199de647b507ee
Jan <jan.rehnert@fau.de>
2023-12-02 12:16:14 +0100

    completed "Find Words That Can Be Formed by Characters"

--- src/Solution.java
+++ src/Solution.java
@@ -4300,4 +4300,19 @@ class Solution {
         }
         return true;
     }
+
+    public int countCharacters(String[] words, String chars) {
+        int[] cnt = new int[26];
+        int ans = 0;
+        chars.chars().forEach(c -> ++cnt[c - 'a']); // count chars.
+        outer:
+        for (String w : words) {
+            int[] count = cnt.clone();
+            for (char c : w.toCharArray())
+                if (--count[c - 'a'] < 0) // not enough, continue next word in words.
+                    continue outer;
+            ans += w.length();
+        }
+        return ans;
+    }
 }
