cdd69572811f208c6646ac49e6166c590b5873a2
Jan <jan.rehnert@fau.de>
2023-12-22 09:36:06 +0100

    completed "Widest Vertical Area Between Two Points Containing No Points"

--- src/Solution.java
+++ src/Solution.java
@@ -4695,4 +4695,13 @@ class Solution {
         }
         return ans;
     }
+
+    public int maxWidthOfVerticalArea(int[][] points) {
+        Arrays.sort(points, Comparator.comparingInt(point -> point[0]));
+        int max = 0;
+        for (int i = 1; i < points.length; i++) {
+            max = Math.max(max, points[i][0] - points[i - 1][0]);
+        }
+        return max;
+    }
 }
