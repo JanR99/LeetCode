fb5fef9d2b231e7884269c6151adac1a4128606f
Jan <jan.rehnert@fau.de>
2023-12-29 17:50:42 +0100

    completed "Find the K-or of an Array"

--- src/Solution.java
+++ src/Solution.java
@@ -4729,4 +4729,16 @@ class Solution {
         }
         return ans;
     }
+
+    public int findKOr(int[] nums, int k) {
+        int ans = 0;
+        for (int i = 0; i <= 31; i++) {
+            int count = 0;
+            for (int num : nums) {
+                if (((num >> i) & 1) != 0) count++;
+            }
+            if (count >= k) ans += (1 << i);
+        }
+        return ans;
+    }
 }
