0230eb89ac0c18a44123e847f083a47b5adb7756
Jan <jan.rehnert@fau.de>
2024-01-14 15:29:23 +0100

    completed "Determine if Two Strings Are Close"

--- src/Solution.java
+++ src/Solution.java
@@ -4961,4 +4961,20 @@ class Solution {
         }
         return nums[0];
     }
+
+    public boolean closeStrings(String word1, String word2) {
+        if (word1.length() != word2.length()) return false;
+        int[] chars1 = new int[26];
+        int[] chars2 = new int[26];
+        for (int i = 0; i < word1.length(); i++) {
+            chars1[word1.charAt(i) - 'a']++;
+            chars2[word2.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < 26; i++)
+            if (chars1[i] == 0 && chars2[i] != 0 || chars1[i] != 0 && chars2[i] == 0)
+                return false;
+        Arrays.sort(chars1);
+        Arrays.sort(chars2);
+        return Arrays.equals(chars1, chars2);
+    }
 }
