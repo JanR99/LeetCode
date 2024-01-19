971314b87a7f41ccd500eb4d54045aede0147011
Jan <jan.rehnert@fau.de>
2024-01-19 09:54:17 +0100

    completed "Minimum Sum of Mountain Triplets I"

--- src/Solution.java
+++ src/Solution.java
@@ -5087,4 +5087,19 @@ class Solution {
         Collections.sort(ans);
         return ans;
     }
+
+    public int minimumSum(int[] nums) {
+        int min = -1;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] < nums[j] && nums[k] < nums[j]) {
+                        int current = nums[i] + nums[j] + nums[k];
+                        if (min > current || min == -1) min = current;
+                    }
+                }
+            }
+        }
+        return min;
+    }
 }
