2e699437accd131106e2a3d628bbbd4b37772394
Jan <jan.rehnert@fau.de>
2024-02-04 01:48:19 +0100

    completed "Type of Triangle II"

--- src/Solution.java
+++ src/Solution.java
@@ -5439,4 +5439,12 @@ class Solution {
         }
         return ans;
     }
+
+    public String triangleType(int[] nums) {
+        if (nums[0] + nums[1] <= nums[2] || nums[0] + nums[2] <= nums[1] || nums[1] + nums[2] <= nums[0])
+            return "none";
+        if (nums[0] == nums[1] && nums[0] == nums[2]) return "equilateral";
+        if (nums[0] == nums[1] || nums[0] == nums[2] || nums[1] == nums[2]) return "isosceles";
+        return "scalene";
+    }
 }
