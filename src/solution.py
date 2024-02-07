48bb86fd7fd91c2734412dc7bd83d0fac4eaf3bb
Jan <jan.rehnert@fau.de>
2024-02-07 20:19:01 +0100

    completed "Double Modular Exponentiation"

---
+++ src/solution.py
@@ -0,0 +1,18 @@
+def getGoodIndices(variables, target):
+    """
+    :type variables: List[List[int]]
+    :type target: int
+    :rtype: List[int]
+    """
+    ans = []
+    for i in range(len(variables)):
+        v = variables[i]
+        a, b, c, m = v[0], v[1], v[2], v[3]
+        if (((a ** b) % 10) ** c) % m == target:
+            ans.append(i)
+    return ans
+
+
+class Solution:
+    def __init__(self):
+        pass
