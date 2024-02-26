f1491dea2f25ddc3139ef131949fbd215b7d3d4a
Jan <jan.rehnert@fau.de>
2024-02-26 22:34:43 +0100

    completed "Maximum Difference Between Increasing Elements"

--- src/Solution.java
+++ src/Solution.java
@@ -5802,4 +5802,17 @@ class Solution {
         }
         return dp[k + 1][dst] == Integer.MAX_VALUE ? -1 : dp[k + 1][dst];
     }
+
+    public int maximumDifference(int[] nums) {
+        int max = -1;
+        int min = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int diff = nums[i] - min;
+            if (diff > 0 && diff > max)
+                max = diff;
+            if (nums[i] < min)
+                min = nums[i];
+        }
+        return max;
+    }
 }
