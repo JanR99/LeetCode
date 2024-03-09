d3a0eeac326aa7df69452e77a3922407a1c89356
Jan <jan.rehnert@fau.de>
2024-03-09 09:43:42 +0100

    completed "Minimum Number of Vertices to Reach All Nodes"

--- src/Solution.java
+++ src/Solution.java
@@ -6005,4 +6005,12 @@ class Solution {
         }
         return ans;
     }
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        boolean[] seen = new boolean[n];
+        for (List<Integer> edge : edges) seen[edge.get(1)] = true;
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            if (!seen[i]) ans.add(i);
+        return ans;
+    }
 }
