2af138d1370a4018f502bcfc47cd577ac96e6401
Jan <jan.rehnert@fau.de>
2024-03-14 09:52:59 +0100

    completed "Semi-Ordered Permutation"

--- src/Solution.java
+++ src/Solution.java
@@ -6061,4 +6061,13 @@ class Solution {
         }
         return newNode.next;
     }
+
+    public int semiOrderedPermutation(int[] nums) {
+        int x = 0, y = 0, n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 1) x = i;
+            else if (nums[i] == n) y = i;
+        }
+        return x > y ? x + (n - y - 1) - 1 : x + (n - y - 1);
+    }
 }
