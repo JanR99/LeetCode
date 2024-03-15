81a6734f783388def143b438ea2f397929a21773
Jan <jan.rehnert@fau.de>
2024-03-15 10:54:37 +0100

    completed "Binary Subarrays With Sum"

--- src/Solution.java
+++ src/Solution.java
@@ -6070,4 +6070,17 @@ class Solution {
         }
         return x > y ? x + (n - y - 1) - 1 : x + (n - y - 1);
     }
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = nums[i];
+            if (sum == goal) ans++;
+            for (int j = i + 1; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == goal) ans++;
+            }
+        }
+        return ans;
+    }
 }
