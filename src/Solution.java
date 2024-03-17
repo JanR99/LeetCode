f0a23a0dcd48fe4674890bba40aea3532aeef60f
Jan <jan.rehnert@fau.de>
2024-03-17 13:24:24 +0100

    completed "Contiguous Array"

--- src/Solution.java
+++ src/Solution.java
@@ -6103,4 +6103,19 @@ class Solution {
         }
         return num;
     }
+
+    public int findMaxLength(int[] nums) {
+        int max = 0;
+        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            count += nums[i] == 1 ? 1 : -1;
+            if (map.containsKey(count))
+                max = Math.max(max, i - map.get(count));
+            else
+                map.put(count, i);
+        }
+        return max;
+    }
 }
