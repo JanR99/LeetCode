91fa6306e2f292c303c3c51c4914f2d74c9750d7
Jan <jan.rehnert@fau.de>
2024-04-14 14:25:07 +0200

    completed "Subsets"

--- src/Solution.java
+++ src/Solution.java
@@ -6570,4 +6570,18 @@ class Solution {
         root.right = bstFromPreorderHelper(preorder, max, end);
         return root;
     }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<>());
+        for (int num : nums) {
+            int n = ans.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> subset = new ArrayList<>(ans.get(i));
+                subset.add(num);
+                ans.add(subset);
+            }
+        }
+        return ans;
+    }
 }
