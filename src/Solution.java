22e7f2a8bd4a7a08d3b331bf292335fd5fe4404c
Jan <jan.rehnert@fau.de>
2024-08-02 15:19:26 +0200

    completed "Find the Number of Good Pairs I"

--- src/Solution.java
+++ src/Solution.java
@@ -6778,4 +6778,14 @@ class Solution {
         }
         return ans;
     }
+
+    public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+        int ans = 0;
+        for (int num1 : nums1) {
+            for (int num2 : nums2) {
+                if (num1 % (num2 * k) == 0) ans++;
+            }
+        }
+        return ans;
+    }
 }
