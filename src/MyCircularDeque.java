80ed797f6f3bfe442c4278d74ebce4b5cda6ba98
Jan <jan.rehnert@fau.de>
2024-09-28 10:33:09 +0200

    completed "Design Circular Deque"

---
+++ src/MyCircularDeque.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+class MyCircularDeque {
+
+    ArrayList<Integer> list;
+    int max;
+
+    public MyCircularDeque(int k) {
+        list = new ArrayList<>(k);
+        max = k;
+    }
+
+    public boolean insertFront(int value) {
+        if (isFull()) return false;
+        list.add(0, value);
+        return true;
+    }
+
+    public boolean insertLast(int value) {
+        if (isFull()) return false;
+        list.add(value);
+        return true;
+    }
+
+    public boolean deleteFront() {
+        if (isEmpty()) return false;
+        list.remove(0);
+        return true;
+    }
+
+    public boolean deleteLast() {
+        if (isEmpty()) return false;
+        list.remove(list.size() - 1);
+        return true;
+    }
+
+    public int getFront() {
+        return list.isEmpty() ? -1 : list.get(0);
+    }
+
+    public int getRear() {
+        return list.isEmpty() ? -1 : list.get(list.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public boolean isFull() {
+        return list.size() == max;
+    }
+}
