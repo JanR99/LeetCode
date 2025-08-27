a31c40e4b3bd32f688749d9cb21633edb2273341
Jan <jan.rehnert@fau.de>
2025-08-27 09:44:52 +0200

    completed "Peeking Iterator"

---
+++ src/PeekingIterator.java
@@ -0,0 +1,26 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
+class PeekingIterator implements Iterator<Integer> {
+
+    private final LinkedList<Integer> stack;
+
+    public PeekingIterator(Iterator<Integer> iterator) {
+        this.stack = new LinkedList<>();
+        iterator.forEachRemaining(stack::add);
+    }
+
+    public Integer peek() {
+        return stack.peek();
+    }
+
+    @Override
+    public Integer next() {
+        return stack.pop();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+}
