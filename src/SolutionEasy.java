144159579f161dc54a00cf162c8d258293364f92
Jan <jan.rehnert@fau.de>
2025-08-29 12:19:12 +0200

    completed "Minimum Cost to Reach Every Position"

--- src/SolutionEasy.java
+++ src/SolutionEasy.java
@@ -17,4 +17,13 @@ public class SolutionEasy {
         }
         return ans;
     }
+
+    public int[] minCosts(int[] cost) {
+        int[] ans = new int[cost.length];
+        ans[0] = cost[0];
+        for (int i = 1; i < ans.length; i++) {
+            ans[i] = Math.min(ans[i - 1], cost[i]);
+        }
+        return ans;
+    }
 }
