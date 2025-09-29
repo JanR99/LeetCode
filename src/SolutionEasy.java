8ac5b50da3522c3c809a76fc14380a08ae3f70bc
Jan <jan.rehnert@fau.de>
2025-09-29 11:31:08 +0200

    completed "Matrix Cells in Distance Order"

--- src/SolutionEasy.java
+++ src/SolutionEasy.java
@@ -162,4 +162,23 @@ public class SolutionEasy {
         }
         return ans;
     }
+
+    public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
+        int[][] ans = new int[rows * cols][2];
+        int index = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                ans[index++] = new int[]{i, j};
+            }
+        }
+
+        Arrays.sort(ans, (a, b) -> {
+            int distA = Math.abs(a[0] - rCenter) + Math.abs(a[1] - cCenter);
+            int distB = Math.abs(b[0] - rCenter) + Math.abs(b[1] - cCenter);
+            return Integer.compare(distA, distB);
+        });
+
+        return ans;
+    }
 }
