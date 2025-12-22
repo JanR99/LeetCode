4382e1e5cef77193c616c21212b1795c58ece12c
Jan <jan.rehnert@fau.de>
2025-12-22 14:00:19 +0100

    completed "Find Missing Elements"

--- src/SolutionEasy.java
+++ src/SolutionEasy.java
@@ -333,4 +333,19 @@ public class SolutionEasy {
         return false;
     }
 
+    public List<Integer> findMissingElements(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (num < min) min = num;
+            if (num > max) max = num;
+            set.add(num);
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int i = min; i < max; i++) {
+            if (!set.contains(i)) ans.add(i);
+        }
+        return ans;
+    }
 }
