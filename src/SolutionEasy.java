a7b8250b0be96d70ff24492f8899fdfa06caab7d
Jan <jan.rehnert@fau.de>
2026-01-17 11:39:04 +0100

    completed "Count Subarrays of Length Three With a Condition"

--- src/SolutionEasy.java
+++ src/SolutionEasy.java
@@ -449,4 +449,14 @@ public class SolutionEasy {
         }
         return maxIndex;
     }
+
+    public int countSubarrays(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            double sum = nums[i] + nums[i + 2];
+            double middle = nums[i + 1] / 2.0;
+            if (sum == middle) ans++;
+        }
+        return ans;
+    }
 }
